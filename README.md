17a5eded927cac462dae2c5f443156cc7f3a07ad
jesuslim <limchuy7@gmail.com>
2023-08-16 13:10:53 -0600

    initial stage

---
+++ README.md
@@ -0,0 +1,8 @@
+# Ionic 6 QR Code
+ QR & Bar Code Generate & Scan - Ionic 6 Angular | Part 1 - QR Code (Capacitor & Cordova Plugins)
+ 
+ Watch the Full Video on YouTube at https://youtu.be/h4LhPiKSPG0
+ 
+ Part 2 video link: https://youtu.be/qGH8TtZuOrw
+
+<img src="https://github.com/Nykz/Ionic-6-qrcode/blob/main/1.png" width="900" height="500" />
---
+++ src/app/home/home.page.ts
@@ -0,0 +1,98 @@
+import { Component, OnDestroy } from '@angular/core';
+// import { BarcodeScanner } from '@awesome-cordova-plugins/barcode-scanner/ngx';
+import { BarcodeScanner } from '@capacitor-community/barcode-scanner';
+import { AlertController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage implements OnDestroy {
+
+  // https://www.npmjs.com/package/angularx-qrcode
+  qrCodeString = 'Codigo QR de prueba para el lector/scanner';
+  scannedResult: any;
+  content_visibility = '';
+
+  constructor(
+    // private barcodeScanner: BarcodeScanner
+    public alertController: AlertController,
+  ) { }
+
+  // startScan() {
+  //   this.barcodeScanner.scan().then(barcodeData => {
+  //     console.log('Barcode data', barcodeData);
+  //     this.scannedResult = barcodeData?.text;
+  //    }).catch(err => {
+  //        console.log('Error', err);
+  //    });
+  // }
+
+  async checkPermission() {
+    try {
+      // check or request permission
+      const status = await BarcodeScanner.checkPermission({ force: true });
+      if (status.granted) {
+        // the user granted permission
+        return true;
+      }
+      return false;
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  async startScan() {
+    try {
+      const permission = await this.checkPermission();
+      if (!permission) {
+        return;
+      }
+      await BarcodeScanner.hideBackground();
+      document.querySelector('body').classList.add('scanner-active');
+      this.content_visibility = 'hidden';
+      const result = await BarcodeScanner.startScan();
+      console.log(result);
+      BarcodeScanner.showBackground();
+      document.querySelector('body').classList.remove('scanner-active');
+      this.content_visibility = '';
+      if (result?.hasContent) {
+        this.scannedResult = result.content;
+        console.log(this.scannedResult);
+
+        if (this.scannedResult == 'Codigo QR de prueba para el lector/scanner') {
+          const alert = await this.alertController.create({
+            header: 'Asistencia tomada!',
+            message: 'Bienvenido con fureza.',
+            buttons: ['Aceptar']
+          });
+          await alert.present();
+        } else {
+          const alert = await this.alertController.create({
+            header: 'Codigo equivocado!',
+            message: 'Prueba de nuevo.',
+            buttons: ['Aceptar']
+          });
+          await alert.present();
+        }
+
+      }
+    } catch (e) {
+      console.log(e);
+      this.stopScan();
+    }
+  }
+
+  stopScan() {
+    BarcodeScanner.showBackground();
+    BarcodeScanner.stopScan();
+    document.querySelector('body').classList.remove('scanner-active');
+    this.content_visibility = '';
+  }
+
+  ngOnDestroy(): void {
+    this.stopScan();
+  }
+
+}
